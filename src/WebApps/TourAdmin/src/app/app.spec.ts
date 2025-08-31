3d1a01fcc0ce6a10f70a6360c3b016253416e712
Darren Powell <darren.powell@hotmail.co.uk>
2025-08-31 20:07:58 +0100

    Create initial Tour Admin app (#1)
    * Create empty angular application
    
    * Update unit tests to match initial application
    
    * Add e2e tests (Playwright)
    
    * Add basic e2e tests

---
+++ src/WebApps/TourAdmin/src/app/app.spec.ts
@@ -0,0 +1,32 @@
+import { TestBed } from '@angular/core/testing';
+import { App } from './app';
+
+describe('App', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(App);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h3')?.textContent).toContain('Tour Admin');
+  });
+
+  it('should render welcome message', () => {
+    const fixture = TestBed.createComponent(App);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('div')?.textContent)
+      .toContain('Welcome to the Tour Admin application');
+  });
+
+});
