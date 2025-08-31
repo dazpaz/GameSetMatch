4c744b33dfefb45c88d50102fd24ebc8250c4872
Darren Powell <darren.powell@hotmail.co.uk>
2025-08-31 19:48:47 +0100

    Add basic e2e tests

--- src/WebApps/TourAdmin/e2e/example.spec.ts
+++ src/WebApps/TourAdmin/e2e/example.spec.ts
@@ -1,8 +1,25 @@
 import { expect, test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+test.describe('basic application tests', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
 
+  test('application has title set correctly', async ({ page }) => {
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('TourAdmin');
+    await expect(page).toHaveTitle('TourAdmin');
+  });
+
+  test('landing of the application page has a title', async ({ page }) => {
+    await expect(page.getByText('Tour Admin', { exact: true })).toBeVisible();
+  });
+
+  test('landing of the application page has a welcome message', async ({ page }) => {
+    await expect(page.getByText('Welcome to the Tour Admin application', { exact: true })).toBeVisible();
+  });
 });
+
+
+
+
+
