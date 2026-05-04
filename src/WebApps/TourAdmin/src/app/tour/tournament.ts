a887ac921ce2d9884e49abe076eb6f20e37cee23
Darren Powell <darren.powell@hotmail.co.uk>
2026-05-04 21:44:07 +0100

    Change Surface and Level to string enums

--- src/WebApps/TourAdmin/src/app/tour/tournament.ts
+++ src/WebApps/TourAdmin/src/app/tour/tournament.ts
@@ -1,16 +1,16 @@
 export enum Surface {
-    Grass,
-    Clay,
-    Hard,
-    Carpet
+    Grass = "Grass",
+    Clay = "Clay",
+    Hard = "Hard",
+    Carpet = "Carpet"
 }
 
 export enum TournamentLevel {
-    GrandSlam,
-    TourFinals,
-    GSM1000,
-    GSM500,
-    GSM250
+    GrandSlam = "Grand Slam",
+    TourFinals = "Tour Finals",
+    GSM1000 = "Masters 1000",
+    GSM500 = "GSM 500",
+    GSM250 = "GSM 250"
 }
 
 export interface ITournament {
