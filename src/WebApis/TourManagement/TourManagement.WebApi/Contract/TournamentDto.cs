1db73adb58e3f43da1b368dfa78193cd347d4c24
Darren Powell <darren.powell@hotmail.co.uk>
2026-05-05 22:29:15 +0100

    Minimal api (#6)
    * Create minimal API to return list of tournament titles
    
    * Add Tournament DTO
    
    * Rename enum values for CoPilot review comment
    
    * Return a hard coded list of tournaments

---
+++ src/WebApis/TourManagement/TourManagement.WebApi/Contract/TournamentDto.cs
@@ -0,0 +1,34 @@
+﻿namespace TourManagement.WebApi.Contract
+{
+	public enum TournamentLevel
+	{
+		GrandSlam,
+		Masters1000,
+		GSM500,
+		GSM250,
+		GSM125
+	}
+
+	public enum Surface
+	{
+		Grass,
+		Clay,
+		Hard,
+		Carpet
+	}
+
+	public class TournamentDto
+	{
+		public Guid TournamentId { get; set; }
+		public required string Title { get; set; }
+		public TournamentLevel Level { get; set; }
+		public Surface Surface { get; set; }
+		public required string Venue { get; set; }
+		public required string Country { get; set; }
+		public bool MensSingles { get; set; }
+		public bool WomensSingles { get; set; }
+		public bool MensDoubles { get; set; }
+		public bool WomensDoubles { get; set; }
+		public bool MixedDoubles { get; set; }
+	}
+}
---
+++ src/WebApis/TourManagement/TourManagement.WebApi/Controllers/TournamentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TourManagement.WebApi.Contract;
+using TourManagement.WebApi.Query;
+
+namespace TourManagement.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TournamentsController : ControllerBase
+	{
+		// GET: api/Tournament
+		[HttpGet]
+		public IEnumerable<TournamentDto> Get()
+		{
+			return GetTournamentList.Execute();
+		}
+	}
+}
