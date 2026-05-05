0e145c41310d4042f8f98c3de2605a0dd1ff5559
Darren Powell <darren.powell@hotmail.co.uk>
2026-05-05 21:47:06 +0100

    Create minimal API to return list of tournament titles

---
+++ src/WebApis/TourManagement/TourManagement.WebApi/Controllers/TournamentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TourManagement.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TournamentsController : ControllerBase
+	{
+		// GET: api/Tournament
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "Australian Open", "French Open", "Wimbledon",
+				"Brisbane International", "Nottingham Open", "Monte-Carlo Masters" };
+		}
+	}
+}
